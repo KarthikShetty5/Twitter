f96a15622f64d8b20dd13a5e000897bba2c65b65
karthikshetty <karhtikshetty1@gmail.com>
2023-07-14 21:16:42 +0530

    error solved

---
+++ app/index.tsx
@@ -0,0 +1,7 @@
+import { Redirect } from "expo-router"
+//this is added bcoz of oner error ,page missing error
+export default function Index() {
+    return (
+        <Redirect href={"/feed"} />
+    )
+}
