fb7e486c437eab03cac350f9bdf3620f7de59a59
karthikshetty <karhtikshetty1@gmail.com>
2023-07-16 22:26:31 +0530

    sign in and auth page

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,6 @@ import { Redirect } from "expo-router"
 //this is added bcoz of oner error ,page missing error
 export default function Index() {
     return (
-        <Redirect href={"/feed"} />
+        <Redirect href={"/signin"} />
     )
 }
